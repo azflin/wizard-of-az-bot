974cbdb14793b2f020a4cf89b2a8b723859b26c9
saul <saul-capital@proton.me>
2024-06-16 03:09:28 +0200

    add ramses!

--- config.ts
+++ config.ts
@@ -3,7 +3,8 @@ export const RPC_URLS: { [key: string]: string } = {
   "pharaoh": "https://avalanche.drpc.org",
   "nuri": "https://scroll.drpc.org",
   "ra": "https://rpc.frax.com",
-  "cleo": "https://mantle.drpc.org"
+  "cleo": "https://mantle.drpc.org",
+  "ramses": "https://arbitrum.drpc.org" 
 };
 
 export const NFPM_ADDRESSES: { [key: string]: string } = {
@@ -11,7 +12,8 @@ export const NFPM_ADDRESSES: { [key: string]: string } = {
   "pharaoh": "0xAAA78E8C4241990B4ce159E105dA08129345946A",
   "nuri": "0xAAA78E8C4241990B4ce159E105dA08129345946A",
   "ra": "0xAAA78E8C4241990B4ce159E105dA08129345946A",
-  "cleo": "0xAAA78E8C4241990B4ce159E105dA08129345946A"
+  "cleo": "0xAAA78E8C4241990B4ce159E105dA08129345946A",
+  "ramses": "0xAA277CB7914b7e5514946Da92cb9De332Ce610EF"
 };
 
 export const CHAIN_IDS: { [key: string]: number } = {
@@ -19,7 +21,8 @@ export const CHAIN_IDS: { [key: string]: number } = {
   "pharaoh": 43114,
   "nuri": 534352,
   "ra": 252,
-  "cleo": 5000
+  "cleo": 5000,
+  "ramses": 42161
 };
 
 export const POOL_INIT_CODE_HASHES: { [key: string]: string } = {
@@ -28,6 +31,7 @@ export const POOL_INIT_CODE_HASHES: { [key: string]: string } = {
   "nuri": "0x1565b129f2d1790f12d45301b9b084335626f0c92410bc43130763b69971135d",
   "ra": "0x1565b129f2d1790f12d45301b9b084335626f0c92410bc43130763b69971135d",
   "cleo": "0x1565b129f2d1790f12d45301b9b084335626f0c92410bc43130763b69971135d",
+  "ramses": "0x1565b129f2d1790f12d45301b9b084335626f0c92410bc43130763b69971135d",
 };
 
 export const FACTORIES: { [key: string]: string } = {
@@ -36,4 +40,5 @@ export const FACTORIES: { [key: string]: string } = {
   "nuri": "0xAAA32926fcE6bE95ea2c51cB4Fcb60836D320C42",
   "ra": "0xAAA32926fcE6bE95ea2c51cB4Fcb60836D320C42",
   "cleo": "0xAAA32926fcE6bE95ea2c51cB4Fcb60836D320C42",
+  "ramses": "0xAA2cd7477c451E703f3B9Ba5663334914763edF8"
 };
